6507ee7cf9188d5d68934d595cb3be82aa35235b
Bogdan B Peterson <github@bogdanbpeterson.com>
2025-01-20 10:21:13 +0200

    fix: don't need raise, can work with assert

--- env.ts
+++ env.ts
@@ -1,10 +1,6 @@
 import { assert } from "@std/assert/assert";
 
-export const raise = (error: string): never => {
-  throw new Error(error);
-};
-
-const PUBLIC_URL = Deno.env.get("PUBLIC_URL") ?? raise("");
+const PUBLIC_URL = Deno.env.get("PUBLIC_URL")!;
 assert(PUBLIC_URL, "Has to have a public URL to work with webhook");
 
 const BOT_TOKEN = Deno.env.get("BOT_TOKEN")!;
