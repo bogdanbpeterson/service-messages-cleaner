8ed7896d06af6582960067dfbe0d9fd0c45423fc
Bogdan B Peterson <github@bogdanbpeterson.com>
2025-01-20 10:20:26 +0200

    feat: adds 'new member' and 'left chat messages' deletion

--- bot.ts
+++ bot.ts
@@ -5,6 +5,14 @@ const bot = new Bot(BOT_TOKEN);
 
 bot.command("start", (ctx) => ctx.reply("Welcome! Add me to a group so I can fulfill my purpose!"));
 
+bot.on([":left_chat_member", ":new_chat_members"], async (ctx) => {
+  try {
+    await ctx.deleteMessage();
+  } catch (err) {
+    console.error("Failed to delete the message:", err);
+  }
+});
+
 const handleUpdates = webhookCallback(bot, "oak");
 
 export { bot, handleUpdates };
